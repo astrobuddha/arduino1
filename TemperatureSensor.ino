6396cc9c9132c5f84879db84a32073518ef01f07
astrobuddha <jozefleo@protonmail.com>
2023-01-10 22:41:30 -0500

    updated temp script

--- TemperatureSensor.ino
+++ TemperatureSensor.ino
@@ -1,24 +1,30 @@
 #include <math.h>
 
-int a;
+const int B = 4275;               // B value of the thermistor
 
-float temperature;
-int B=3975; // B value of Thermistor
-float resistance;
+const int R0 = 100000;            // R0 = 100k
 
+const int pinTempSensor = A0;     // Grove - Temperature Sensor connect to A0
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    a = analogRead(0);
-    resistance = (float) (1023 - a) * 10000 / a; // get the resistance of the sensor;
-    temperature = 1 / (log(resistance / 10000) / B + 1 / 298.15) - 273/15; // convert to temperature via datasheet;
+    int a = analogRead(pinTempSensor);
+ 
+    float R = 1023.0/a-1.0;
+    R = R0*R;
+ 
+    float tempC = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
+
+    float tempF = (tempC * 9.0/5.0) + 32;
+ 
+    Serial.print("temp in F = ");
+    Serial.println(tempF);
+
     delay(1000);
-    Serial.print("current temperature is ");
-    Serial.println(temperature);
 }
 
 
